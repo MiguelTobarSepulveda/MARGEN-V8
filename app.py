e89b82ed89e809f5aa6f00dcaa33148ac770960c
MiguelTobarSepulveda <mtobar@armony.cl>
2025-06-24 18:21:15 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -20,8 +20,7 @@ authenticator = stauth.Authenticate(
     config['credentials'],
     config['cookie']['name'],
     config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    auto_hash=True
+    config['cookie']['expiry_days']
 
 )
 
